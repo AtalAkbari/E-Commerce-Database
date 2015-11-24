b1625f7356919d7ddeb58632cc9ad1ca37397d56
Atal Akbari <atal.akbari@hotmail.com>
2015-11-24 18:13:49 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,17 @@
+<!-- Connect to Oracle --> 
 
+<?php		
+         //Connect to Oracle       
+		$conn = oci_connect("USERNAME", "PASSWORD", "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.scs.ryerson.ca)(PORT=1521))(CONNECT_DATA=(SID=orcl)))");
+        
+        if (!$conn)
+			{
+                    $m = oci_error();
+                    echo $m['message'], "\n";
+                     exit;
+            } 
+       else 
+	   {
+                 
+        }	            
+?>
